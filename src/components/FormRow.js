369cbeaa57470fec385f4d6e5578f4d5d05e34c3
eglereact <eglereactjs@gmail.com>
2022-05-20 15:45:48 +0300

    style register page

---
+++ src/components/FormRow.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+function FormRow({
+  label,
+  type,
+  name,
+  value,
+  handleChange,
+  icon,
+  placeholder,
+}) {
+  return (
+    <div className="w-full mt-3">
+      <label
+        htmlFor={name}
+        className="block mb-1 text-sm font-medium text-gray-800 uppercase"
+      >
+        {label}
+      </label>
+      <div className="relative">
+        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none text-gray-800 text-lg">
+          {icon}
+        </div>
+        <input
+          type={type}
+          name={name}
+          value={value}
+          onChange={handleChange}
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  w-full outline-none pl-10 p-2.5"
+          placeholder={placeholder}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default FormRow;
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,5 +1,101 @@
+import { useState, useEffect } from "react";
+import { Logo, FormRow } from "./../components";
+import { AiOutlineMail, AiOutlineLock, AiOutlineUser } from "react-icons/ai";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+};
+
 function Register() {
-  return <div>Register</div>;
+  const [values, setValues] = useState(initialState);
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setValues({ ...values, [name]: value });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const { isMember, name, email, password } = values;
+    if (!email || !password || (!isMember && !name)) {
+      console.log("Please fill out all fields");
+    }
+  };
+
+  const toggleMember = () => {
+    setValues({ ...values, isMember: !values.isMember });
+  };
+
+  return (
+    <div className="bg-gray-200 h-screen flex items-center justify-center">
+      <form
+        onSubmit={onSubmit}
+        className="w-[90vw] max-w-[400px] flex flex-col items-center justify-center border
+        bg-gray-100 border-gray-300 rounded-lg p-5 shadow-md "
+      >
+        <Logo />
+        <h3 className="text-2xl font-bold text-[#A07265] my-2">
+          {values.isMember ? "Login" : "Register"}
+        </h3>
+        {!values.isMember && (
+          <FormRow
+            icon={<AiOutlineUser />}
+            type="text"
+            name="name"
+            label="Name"
+            handleChange={handleChange}
+            placeholder="Jonny"
+            value={values.name}
+          />
+        )}
+
+        <FormRow
+          icon={<AiOutlineMail />}
+          type="email"
+          name="email"
+          label="Email"
+          handleChange={handleChange}
+          placeholder="name@example.com"
+          value={values.email}
+        />
+        <FormRow
+          icon={<AiOutlineLock />}
+          type="password"
+          name="password"
+          label="Password"
+          handleChange={handleChange}
+          placeholder="********"
+          value={values.password}
+        />
+        <button
+          type="submit"
+          className="relative inline-block px-6 py-3 font-bold text-[#2F2E41] group mt-8"
+        >
+          <span
+            className="absolute inset-0 w-full h-full transition duration-300 ease-out transform -translate-x-2 -translate-y-2
+             bg-[#A07265] group-hover:translate-x-0 group-hover:translate-y-0"
+          ></span>
+          <span className="absolute inset-0 w-full h-full border-4 border-[#2F2E41]"></span>
+          <span className="relative">Submit</span>
+        </button>
+        <p className="text-gray-800 mt-5">
+          {values.isMember ? "Not a member yet? " : "Already a member? "}
+
+          <button
+            type="button"
+            onClick={toggleMember}
+            className="text-[#A07265] font-bold hover:text-[#2F2E41]"
+          >
+            {values.isMember ? "Register" : "Login"}
+          </button>
+        </p>
+      </form>
+    </div>
+  );
 }
 
 export default Register;
